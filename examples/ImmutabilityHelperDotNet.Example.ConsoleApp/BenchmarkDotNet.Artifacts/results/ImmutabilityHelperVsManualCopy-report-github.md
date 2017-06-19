1e89f521db75f53226469b0d79990b68576ac574
rflechner <rflechner@romcyber.com>
2017-06-19 10:35:35 +0200

    TASK: generate benchmarks

--- examples/ImmutabilityHelperDotNet.Example.ConsoleApp/BenchmarkDotNet.Artifacts/results/ImmutabilityHelperVsManualCopy-report-github.md
+++ examples/ImmutabilityHelperDotNet.Example.ConsoleApp/BenchmarkDotNet.Artifacts/results/ImmutabilityHelperVsManualCopy-report-github.md
@@ -1,15 +1,15 @@
 ``` ini
 
-BenchmarkDotNet=v0.10.8, OS=Windows 10 Redstone 1 (10.0.14393)
-Processor=Intel Core i7-4770 CPU 3.40GHz (Haswell), ProcessorCount=8
-Frequency=3318397 Hz, Resolution=301.3503 ns, Timer=TSC
+BenchmarkDotNet=v0.10.8, OS=Windows 10 Redstone 2 (10.0.15063)
+Processor=Intel Core i7-6560U CPU 2.20GHz (Skylake), ProcessorCount=4
+Frequency=2156249 Hz, Resolution=463.7683 ns, Timer=TSC
 dotnet cli version=1.0.4
   [Host]     : .NET Core 4.6.25211.01, 64bit RyuJITDEBUG [AttachedDebugger]
   DefaultJob : .NET Core 4.6.25211.01, 64bit RyuJIT
 
 
 ```
- |     Method |        Mean |      Error |     StdDev |
- |----------- |------------:|-----------:|-----------:|
- | ManualCopy |    19.57 ns |  0.4268 ns |  0.4192 ns |
- | HelperCopy | 4,799.53 ns | 41.7243 ns | 39.0290 ns |
+ |     Method |        Mean |       Error |     StdDev |
+ |----------- |------------:|------------:|-----------:|
+ | ManualCopy |    26.28 ns |   0.6541 ns |   1.592 ns |
+ | HelperCopy | 7,206.77 ns | 173.0060 ns | 212.467 ns |
--- examples/ImmutabilityHelperDotNet.Example.ConsoleApp/Program.cs
+++ examples/ImmutabilityHelperDotNet.Example.ConsoleApp/Program.cs
@@ -1,32 +1,10 @@
 ﻿using System;
-using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using ImmutabilityHelperDotNet.Example.Domain;
 
 namespace ImmutabilityHelperDotNet.Example.ConsoleApp
 {
-    public class Md5VsSha256
-    {
-        private const int N = 10000;
-        private readonly byte[] data;
-
-        private readonly SHA256 sha256 = SHA256.Create();
-        private readonly MD5 md5 = MD5.Create();
-
-        public Md5VsSha256()
-        {
-            data = new byte[N];
-            new Random(42).NextBytes(data);
-        }
-
-        [Benchmark]
-        public byte[] Sha256() => sha256.ComputeHash(data);
-
-        [Benchmark]
-        public byte[] Md5() => md5.ComputeHash(data);
-    }
-
     public class ImmutabilityHelperVsManualCopy
     {
         DebtorModel d1 = new DebtorModel("Toto", 42);
@@ -46,16 +24,14 @@ namespace ImmutabilityHelperDotNet.Example.ConsoleApp
 
 
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 1000000; i++)
             {
                 var d2 = d1
                     .With(d => d.Name, "tutu")
                     .With(d => d.Age, 45)
                     .Clone();
             }
-
-
-            //var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            
             //var summary = BenchmarkRunner.Run<ImmutabilityHelperVsManualCopy>();
 
             Console.WriteLine("Press any key to quit ...");
